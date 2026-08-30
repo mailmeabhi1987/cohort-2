08461d90056f098791e7da2874368d90d4039545
mailmeabhi1987 <mailmeabhi1987@gmail.com>
2026-08-30 23:38:58 +0530

    MAC-OS portfolio project created

---
+++ MAC-OS/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import "./app.scss"
+import Dock from './components/Dock'
+import Nav from './components/Nav'
+import MacWindow from './components/windows/MacWindow'
+
+function App() {
+
+  return (
+    <main>
+      <Nav />
+      <Dock />
+      <MacWindow>
+        
+      </MacWindow>
+    </main>
+  )
+}
+
+export default App
---
+++ MAC-OS/src/components/windows/Github.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import MacWindow from './MacWindow'
+
+const Github = () => {
+  return (
+    <MacWindow>
+    </MacWindow>
+  )
+}
+
+export default Github
