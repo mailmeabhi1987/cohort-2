2e13ee1d59d00a8288cf525f65b598a827c5da4b
mailmeabhi1987 <mailmeabhi1987@gmail.com>
2026-09-22 23:21:02 +0530

    layout and pages, dynamic routes create in next app

---
+++ NextJs/event-project/app/page.tsx
@@ -0,0 +1,12 @@
+import Hello from "@/app/components/hello";
+
+const Home = () => {
+    console.log("what type of component am i");
+    return (
+        <main>
+            <div className="text-5xl underline">Welcome to next js</div>
+
+        </main>
+    )
+}
+export default Home
